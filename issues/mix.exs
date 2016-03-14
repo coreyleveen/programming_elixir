5c87ced560364cad2ce62f0ec168b2641b211874
Corey Leveen <mntj11@gmail.com>
2016-03-13 20:43:34 -0400

    Documentation

--- issues/mix.exs
+++ issues/mix.exs
@@ -30,7 +30,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       { :httpoison, "~> 0.8" },
-      { :poison,    "~> 1.5" }
+      { :poison,    "~> 1.5" },
+      { :ex_doc,    "~> 0.11"},
+      { :earmark,  ">= 0.0.0"}
     ]
   end
 
